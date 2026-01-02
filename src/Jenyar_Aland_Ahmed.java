180b25623a0800df3a885fd25a2a592ba7efae0e
JenyarS <jenyar21@gmail.com>
2026-01-02 20:27:33 +0300

    added arrays to store events/categories

--- src/Jenyar_Aland_Ahmed.java
+++ src/Jenyar_Aland_Ahmed.java
@@ -3,6 +3,15 @@ public class Jenyar_Aland_Ahmed {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args){
+        // single dimension array to store the categories
+        String[] category = {"Music", "Art Exhibitions", "Literature & Poetry"};
+        // two dimension array to store the events as [category][event]
+        String[][] event = {
+                {"cat1Event1", "cat1Event2"},
+                {"cat2Event1", "cat2Event2"},
+                {"cat3Event1", "cat3Event2"}
+        };
+
         int choice = menu();
 
         switch (choice){
